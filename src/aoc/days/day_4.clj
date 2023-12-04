4a4d29b15ea99774c914b244c5ea521d5810122e
bh34e5 <b.hester0614@yahoo.com>
2023-12-04 01:28:25 -0500

    day4 part2

--- src/aoc/days/day_4.clj
+++ src/aoc/days/day_4.clj
@@ -36,3 +36,43 @@
   (let [lines (read-as-lines)
         parsed (parse-lines lines)]
     (reduce + (map card-score parsed))))
+
+;;; part 2
+
+(defn- additional-cards
+  [grouped-cards vec-ct overlap-ct card]
+  (let [copy-nums (map (partial + card)
+                       (map inc (range overlap-ct)))
+        card-arrs (map (partial get grouped-cards) copy-nums)
+        dup-cards (map #(repeat vec-ct (first %1)) card-arrs)
+        vectorized (mapcat #(vector (:card (first %1)) %1)
+                           dup-cards)]
+    vectorized))
+
+(defn- process-copies [grouped-cards]
+  (loop [grouped-cards grouped-cards
+         card 1
+         res {}]
+    (let [card-vec (get grouped-cards card)]
+      (if (<= card (count grouped-cards))
+        (let [c (first card-vec)
+              vec-ct (count card-vec)
+              overlap (set/intersection (:wins c) (:mine c))
+              overlap-ct (count overlap)
+              copies (additional-cards grouped-cards
+                                       vec-ct
+                                       overlap-ct
+                                       (:card c))
+              copies-map (apply hash-map copies)
+              merged (merge-with concat grouped-cards copies-map)]
+          (recur merged
+                 (inc card)
+                 (into res (hash-map card card-vec))))
+        res))))
+
+(defn part-2 []
+  (let [lines (read-as-lines)
+        parsed (parse-lines lines)
+        grouped (group-by :card parsed)
+        processed (process-copies grouped)]
+    (reduce + (map count (vals processed)))))
